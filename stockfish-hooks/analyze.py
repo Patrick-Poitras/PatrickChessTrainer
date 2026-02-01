1220cbb67a4822a02db0648280b0df613fc8ca34
Patrick Poitras <patrick@pfpoitras.com>
2026-02-01 11:10:06 -0800

    Finalize the analysis script

--- stockfish-hooks/analyze.py
+++ stockfish-hooks/analyze.py
@@ -3,19 +3,34 @@ import chess.pgn
 import chess.engine
 import io
 import time
+import concurrent.futures
 
-pgn = open(f"lichess_{uname}_2026-01-31.pgn")
+pgn_path = f"lichess_{uname}_2026-01-31.pgn"
+stockfish_path = "stockfish/stockfish-ubuntu-x86-64-avx2"
 
-games = []
-game = chess.pgn.read_game(pgn)
-while game is not None:
-    games.append(str(game))
+# debugging for games that don't return
+debug_when_clogged = True
+clogging_threshold = 900 # games start printing debug info after 15 mins
+
+def read_all_games(path):
+    pgn = open(path)
+    games = []
     game = chess.pgn.read_game(pgn)
 
+    # limit for debugging. -1 disables the limit
+    limit = -1
+    while game is not None:
+        games.append(str(game))
+        game = chess.pgn.read_game(pgn)
+        if limit == len(games):
+            break
+    return games
+
+games = read_all_games(pgn_path)
 
 def process_game(game_str, debug=False):
     starttime = time.time()
-    eng = chess.engine.SimpleEngine.popen_uci("stockfish/stockfish-ubuntu-x86-64-avx2")
+    eng = chess.engine.SimpleEngine.popen_uci(stockfish_path)
     game = chess.pgn.read_game(io.StringIO(game_str))
     print("Starting processing of", game.headers["Date"] ,game.headers["UTCTime"])
     #mainly analysis
@@ -28,33 +43,37 @@ def process_game(game_str, debug=False):
         assert(False)
 
     for node in game.mainline():
-        info = eng.analyse(node.board(), chess.engine.Limit(depth=20))
+        info = eng.analyse(node.board(), chess.engine.Limit(depth=20, time=15))
         score_pov = info["score"]
         if not score_pov.is_mate():
-            if info["time"] < 0.1 and info["depth"] < 50:
-                info = eng.analyse(node.board(), chess.engine.Limit(time=1))
+            if info.get("time", 9999) < 0.1:
+                info = eng.analyse(node.board(), chess.engine.Limit(time=1, depth=50))
             
         score_me = (score_pov.white() if color == "white" else score_pov.black())
-        node.set_eval(score_pov,info["depth"])
+        node.set_eval(score_pov, info["depth"])
+        if time.time() - starttime > clogging_threshold:
+            debug = debug_when_clogged
         if debug:
-            print(str((node.ply()+1)//2)+("." if node.ply() % 2 == 1 else "..."), node.san(), score_me, info["depth"], info.get("time",None))
+            move_str = str((node.ply()+1)//2)+("." if node.ply() % 2 == 1 else "...") 
+            print(move_str, node.san(), score_me, info["depth"], info.get("time", None))
     print("Done processing of", game.headers["Date"] ,game.headers["UTCTime"], "Time:", time.time()-starttime)
     eng.close()
     return str(game)
 
-#print(process_game(str(games[2], debug=True)))
+#print(process_game(str(games[28]), debug=True))
 
-import concurrent.futures
 done_games = []
 def run():
     outfile = open("processed-games", "w")
     print("Starting runs")
+    starttime = time.time()
     with concurrent.futures.ProcessPoolExecutor(max_workers=24) as executor:
         for value in executor.map(process_game, games):
             done_games.append(value)
-            print(f"Done! {len(done_games)}/{len(games)}")
+            speed = len(done_games)/(time.time()-starttime)
             print(value, file=outfile, end="\n\n")
-        print("Done!")
+            print(f"Done indexing! {len(done_games)}/{len(games)}", "Time remaining:", (len(games)-len(done_games))/speed)
+        print("All complete!")
     outfile.close()
 
 if __name__ == "__main__":
